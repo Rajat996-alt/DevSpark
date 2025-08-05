a4f09f4dc36b3c170d3b040139ae786bdc9e4446
Rajat Sen <rajatsen7568@gmail.com>
2025-08-05 09:59:46 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,22 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.get(/a/, (req, res) => {
-  res.send("I am the new user.");
-});
-
-app.post("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Rajat", lastName: "Sen" });
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Home page!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    //Route handler
+    console.log("1st response handler!");
+    //res.send("1st response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd response handler!");
+    //res.send("2nd response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd response handler!");
+    //res.send("3rd response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th response handler!");
+    //res.send("4th response!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("5th response handler!");
+    res.send("5th response!");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening to port 3000...");
