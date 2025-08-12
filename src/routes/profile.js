4c553c37d9a4a3b20f457c5fe241d9ac82f014d8
Rajat Sen <rajatsen7568@gmail.com>
2025-08-12 09:38:21 +0530

    Created updatePassword route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,8 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateProfileEditData } = require("../utils/validate");
-const e = require("express");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -31,6 +32,37 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       message: `${loggedInUser.firstName}, your profile updated succcessfully`,
       data: loggedInUser,
     });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/updatePassword", userAuth, async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    if (!password || !newPassword) {
+      return res.status(400).json({ message: "Both the fields are required!" });
+    }
+
+    const loggedInUser = req.user;
+
+    const isMatch = await bcrypt.compare(password, loggedInUser.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "The password is incorrect." });
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error(
+        "Password must be strong (min 8 chars, at least 1 uppercase, 1 lowercase, 1 number, and 1 symbol)."
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+
+    res.json({ message: "Password updated successfully!" });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
